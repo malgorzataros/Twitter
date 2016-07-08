e268fba551f88ba24d19834a1008fb044ac62fdb
malgorzataros <malgorzataros@wp.pl>
2016-07-08 10:11:51 +0000

    Katalog do laczenia z baza danych

---
+++ src/Connection.php
@@ -0,0 +1,19 @@
+<?php
+
+$servername="localhost";
+$username="root";
+$password="coderslab"; 
+$baseName= "twitter";   
+
+//Tworzenie nowego połączenia
+$conn=new mysqli($servername, $username, $password, $baseName);
+
+//sprwadzenie poprawności połączenia
+if($conn->connect_error){
+    die("Blad przy polaczeiu do baz danych: $conn->connect_error");
+}
+
+$conn->set_charset("utf8");
+echo("Polaczenie z baza danych udane. <br>");
+
+
